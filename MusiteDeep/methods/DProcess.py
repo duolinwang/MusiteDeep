3d61ae53871e2eda7872ca54c9c66363a4d85b7e
duolinwang <deepduoduo@163.com>
2017-08-16 22:03:08 -0500

    Update DProcess.py

--- MusiteDeep/methods/DProcess.py
+++ MusiteDeep/methods/DProcess.py
@@ -172,120 +172,6 @@ def convertSampleToVector2DList(sampleSeq3DArr, nb_windows, refMatrFileName):
 
 	return probMatr_list
 
-def convertSampleToPhysicsVector(sampleSeq3DArr):
-	"""
-	Convertd the raw data to physico-chemical property
-	
-	PARAMETER
-	---------
-	sampleSeq3DArr: 3D numpy array
-		X denoted the unknow amino acid.
-	
-	
-	probMatr: Probability Matrix for Samples. Shape (nb_samples, 1, nb_length_of_sequence, nb_AA)
-	"""
-	
-	letterDict = {} #hydrophobicty, hydrophilicity, side-chain mass, pK1 (alpha-COOH), pK2 (NH3), PI, Average volume of buried residue, Molecular weight, Side chain volume, Mean polarity
-	letterDict["A"] = [0.62,	-0.5,	15,	2.35,	9.87,	6.11,	91.5,	89.09,	27.5,	-0.06]
-	letterDict["C"] = [0.2900,	-1.0000,	47.0000,    1.7100,   10.7800,    5.0200,	117.7,	121.15,	44.6,	1.36]
-	letterDict["D"] = [-0.9000,    3.0000,   59.0000,    1.8800,    9.6000,    2.9800,	124.5,	133.1,	40,	-0.8]
-	letterDict["E"] = [-0.7400,    3.0000,   73.0000,    2.1900,    9.6700,    3.0800,	155.1,	147.13,	62,	-0.77]
-	letterDict["F"] = [1.1900,   -2.5000,   91.0000,    2.5800,    9.2400,    5.9100,	203.4,	165.19,	115.5,	1.27]
-	letterDict["G"] = [0.4800,         0,    1.0000,    2.3400,    9.6000,    6.0600,	66.4,	75.07,	0,	-0.41]
-	letterDict["H"] = [-0.4000,   -0.5000,   82.0000,    1.7800,    8.9700,    7.6400,	167.3,	155.16,	79,	0.49]
-	letterDict["I"] = [1.3800,   -1.8000,   57.0000,    2.3200,    9.7600,    6.0400,	168.8,	131.17,	93.5,	1.31]
-	letterDict["K"] = [-1.5000,    3.0000,   73.0000,    2.2000,    8.9000,    9.4700,	171.3,	146.19,	100,	-1.18]
-	letterDict["L"] = [1.0600,   -1.8000,   57.0000,    2.3600,    9.6000,    6.0400,	167.9,	131.17,	93.5,	1.21]
-	letterDict["M"] = [0.6400,   -1.3000,   75.0000,    2.2800,    9.2100,    5.7400,	170.8,	149.21,	94.1,	1.27]
-	letterDict["N"] = [-0.7800,    0.2000,   58.0000,    2.1800,    9.0900,   10.7600,	135.2,	132.12,	58.7,	-0.48]
-	letterDict["P"] = [0.1200,         0,   42.0000,    1.9900,   10.6000,    6.3000,	129.3,	115.13,	41.9,	0]
-	letterDict["Q"] = [-0.8500,    0.2000,   72.0000,    2.1700,    9.1300,    5.6500,	161.1,	146.15,	80.7,	-0.73]
-	letterDict["R"] = [-2.5300,    3.0000,  101.0000,    2.1800,    9.0900,   10.7600,	202,	174.2,	105,	-0.84]
-	letterDict["S"] = [-0.1800,    0.3000,   31.0000,    2.2100,    9.1500,    5.6800,	99.1,	105.09,	29.3,	-0.5]
-	letterDict["T"] = [-0.0500,   -0.4000,   45.0000,    2.1500,    9.1200,    5.6000,	122.1,	119.12,	51.3,	-0.27]	
-	letterDict["V"] = [1.0800,   -1.5000,   43.0000,    2.2900,    9.7400,    6.0200,	141.7,	117.15,	71.5,	1.09]
-	letterDict["W"] = [0.8100,   -3.4000,  130.0000,    2.3800,    9.3900,    5.8800,	237.6,	204.24,	145.5,	0.88]
-	letterDict["Y"] = [0.2600,   -2.3000,  107.0000,    2.2000,    9.1100,    5.6300,	203.6,	181.19,	117.3,	0.33]
-	AACategoryLen = 10
-	
-	probMatr = np.zeros((len(sampleSeq3DArr), 1, len(sampleSeq3DArr[0]), AACategoryLen))
-	
-	
-	sampleNo = 0
-	for sequence in sampleSeq3DArr:
-	
-		AANo	 = 0
-		for AA in sequence:
-			
-			if not AA in letterDict:
-				probMatr[sampleNo][0][AANo] = np.full((1,AACategoryLen), 0)
-			
-			else:
-				probMatr[sampleNo][0][AANo]= letterDict[AA]
-				
-			AANo += 1
-		sampleNo += 1
-	
-	return probMatr
-
-
-def convertSampleToPhysicsVector_2(sampleSeq3DArr):
-	"""
-	Convertd the raw data to physico-chemical property
-	
-	PARAMETER
-	---------
-	sampleSeq3DArr: 3D numpy array
-		X denoted the unknow amino acid.
-	
-	
-	probMatr: Probability Matrix for Samples. Shape (nb_samples, 1, nb_length_of_sequence, nb_AA)
-	"""
-	
-	letterDict = {} 
-	letterDict["A"] = [-0.591, -1.302, -0.733, 1.570,-0.146]
-	letterDict["C"] = [ -1.343, 0.465, -0.862, -1.020, -0.255]
-	letterDict["D"] = [1.050, 0.302, -3.656, -0.259, -3.242]
-	letterDict["E"] = [1.357, -1.453, 1.477, 0.113, -0.837]
-	letterDict["F"] = [-1.006, -0.590, 1.891, -0.397, 0.412]
-	letterDict["G"] = [-0.384, 1.652, 1.330, 1.045, 2.064]
-	letterDict["H"] = [0.336, -0.417, -1.673, -1.474, -0.078]
-	letterDict["I"] = [-1.239, -0.547, 2.131, 0.393, 0.816]
-	letterDict["K"] = [1.831, -0.561, 0.533, -0.277, 1.648]
-	letterDict["L"] = [-1.019, -0.987, -1.505, 1.266, -0.912]
-	letterDict["M"] = [-0.663, -1.524, 2.219, -1.005, 1.212]
-	letterDict["N"] = [0.945, 0.828, 1.299, -0.169, 0.933]
-	letterDict["P"] = [0.189, 2.081, -1.628, 0.421, -1.392]
-	letterDict["Q"] = [0.931, -0.179, -3.005, -0.503, -1.853]
-	letterDict["R"] = [1.538, -0.055, 1.502, 0.440, 2.897]
-	letterDict["S"] = [-0.228, 1.399, -4.760, 0.670, -2.647]
-	letterDict["T"] = [-0.032, 0.326, 2.213, 0.908, 1.313]	
-	letterDict["V"] = [-1.337, -0.279, -0.544, 1.242, -1.262]
-	letterDict["W"] = [-0.595, 0.009, 0.672, -2.128, -0.184]
-	letterDict["Y"] = [0.260, 0.830, 3.097, -0.838, 1.512]
-	AACategoryLen = 5
-	
-	probMatr = np.zeros((len(sampleSeq3DArr), 1, len(sampleSeq3DArr[0]), AACategoryLen))
-	
-	
-	sampleNo = 0
-	for sequence in sampleSeq3DArr:
-	
-		AANo	 = 0
-		for AA in sequence:
-			
-			if not AA in letterDict:
-				probMatr[sampleNo][0][AANo] = np.full((1,AACategoryLen), 0)
-			
-			else:
-				probMatr[sampleNo][0][AANo]= letterDict[AA]
-				
-			AANo += 1
-		sampleNo += 1
-	
-	return probMatr
-
-
 def convertSampleToDoubleVec(sampleSeq3DArr, nb_neibor):
     letterDict = {}
     letterDict["A"] = 0
@@ -374,10 +260,6 @@ def convertRawToXY(rawDataFrame, refMatrFileName="", nb_windows=3, codingMode=0)
         probMatr = convertSampleToVector2DList(sampleSeq3DArr, nb_windows, refMatrFileName)
     elif codingMode == 2:
         probMatr = convertSampleToDoubleVec(sampleSeq3DArr, 1)
-    elif codingMode == 3:
-        probMatr = convertSampleToPhysicsVector(sampleSeq3DArr)
-    elif codingMode == 4:
-        probMatr = convertSampleToPhysicsVector_2(sampleSeq3DArr)
     return probMatr, targetArr
      
 
