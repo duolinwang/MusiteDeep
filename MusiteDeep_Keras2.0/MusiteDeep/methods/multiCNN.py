b41892e76b4acfe1db8d3b4242709af227277c68
duolinwang <deepduoduo@163.com>
2018-03-08 00:53:08 -0600

    Add files via upload

--- MusiteDeep_Keras2.0/MusiteDeep/methods/multiCNN.py
+++ MusiteDeep_Keras2.0/MusiteDeep/methods/multiCNN.py
@@ -18,8 +18,67 @@ from keras.optimizers import SGD
 from keras.layers.normalization import BatchNormalization
 from keras.regularizers import l1, l2
 from attention import Attention,myFlatten
-import copy
 
+
+#def copy_model(model):
+#    config=model.get_config()
+#    cp_model = Model.from_config(config)
+#    return cp_model
+
+def copy_model(input_row,input_col):
+         
+         input = Input(shape=(input_row,input_col))
+         filtersize1=1
+         filtersize2=9
+         filtersize3=10
+         filter1=200
+         filter2=150
+         filter3=200
+         dropout1=0.75
+         dropout2=0.75
+         dropout4=0.75
+         dropout5=0.75
+         dropout6=0
+         L1CNN=0
+         nb_classes=2
+         batch_size=1200
+         actfun="relu"; 
+         optimization='adam';
+         attentionhidden_x=10
+         attentionhidden_xr=8
+         attention_reg_x=0.151948
+         attention_reg_xr=2
+         dense_size1=149
+         dense_size2=8
+         dropout_dense1=0.298224
+         dropout_dense2=0
+         input = Input(shape=(input_row,input_col))
+         x = conv.Convolution1D(filter1, filtersize1,kernel_initializer='he_normal',kernel_regularizer= l1(L1CNN),padding="same")(input) 
+         x = Dropout(dropout1)(x)
+         x = Activation(actfun)(x)
+         x = conv.Convolution1D(filter2,filtersize2,kernel_initializer='he_normal',kernel_regularizer= l1(L1CNN),padding="same")(x)
+         x = Dropout(dropout2)(x)
+         x = Activation(actfun)(x)
+         x = conv.Convolution1D(filter3,filtersize3,kernel_initializer='he_normal',kernel_regularizer= l1(L1CNN),padding="same")(x)
+         x = Activation(actfun)(x)
+         x_reshape=core.Reshape((x._keras_shape[2],x._keras_shape[1]))(x)
+         x = Dropout(dropout4)(x)
+         x_reshape=Dropout(dropout5)(x_reshape)
+         decoder_x = Attention(hidden=attentionhidden_x,activation='linear',init='he_normal',W_regularizer=l1(attention_reg_x)) # success  
+         decoded_x=decoder_x(x)
+         output_x = myFlatten(x._keras_shape[2])(decoded_x)
+         decoder_xr = Attention(hidden=attentionhidden_xr,activation='linear',init='he_normal',W_regularizer=l1(attention_reg_xr))
+         decoded_xr=decoder_xr(x_reshape)
+         output_xr = myFlatten(x_reshape._keras_shape[2])(decoded_xr)
+         output=merge([output_x,output_xr],mode='concat')
+         output=Dropout(dropout6)(output)
+         output=Dense(dense_size1,kernel_initializer='he_normal',activation='relu')(output)
+         output=Dropout(dropout_dense1)(output)
+         output=Dense(dense_size2,activation="relu",kernel_initializer='he_normal')(output)
+         output=Dropout(dropout_dense2)(output)
+         out=Dense(nb_classes,kernel_initializer='he_normal',activation='softmax')(output)
+         cp_model=Model(input,out)
+         return cp_model
 
 def MultiCNN(trainX, trainY,valX=None, valY=None,
              batch_size=1200, 
@@ -108,7 +167,8 @@ def MultiCNN(trainX, trainY,valX=None, valY=None,
             if not forkinas:
                  cnn.load_weights(weights);
             else:
-                 cnn2=copy.deepcopy(cnn)
+                 #cnn2=copy_model(cnn)
+                 cnn2=copy_model(input_row,input_col)
                  cnn2.load_weights(weights);
                  for l in range((len(cnn2.layers)-transferlayer)): #the last cnn is not included
                     cnn.layers[l].set_weights(cnn2.layers[l].get_weights())
