5b329add6bca8bf8890f922bd001949f50b0aa24
duolinwang <wangdu@missouri.edu>
2019-04-18 15:58:08 -0500

    modified attention

--- MusiteDeep_Keras2.0/MusiteDeep/methods/attention.py
+++ MusiteDeep_Keras2.0/MusiteDeep/methods/attention.py
@@ -7,25 +7,28 @@ from keras.engine import InputSpec
 class Attention(Layer):
     
     def __init__(self,hidden,init='glorot_uniform',activation='linear',W_regularizer=None,W0_regularizer=None,W_constraint=None,W0_constraint=None,**kwargs):
-        self.W_initializer = initializers.get(init)
+        super(Attention, self).__init__(**kwargs)
+        self.init = initializers.get(init)
         self.activation = activations.get(activation)
         self.W_regularizer = regularizers.get(W_regularizer)
         self.W0_regularizer = regularizers.get(W0_regularizer)
         self.W_constraint = constraints.get(W_constraint)
         self.W0_constraint = constraints.get(W0_constraint)
         self.hidden=hidden
-        super(Attention, self).__init__(**kwargs)
+        
         
     def build(self, input_shape):
+        super(Attention, self).build(input_shape)
         input_dim = input_shape[-1]
         self.input_length = input_shape[1]
-        self.W0 = self.add_weight(shape=(input_dim, self.hidden), name='W0',initializer=self.W_initializer,regularizer=self.W0_regularizer,constraint=self.W0_constraint)
-        self.W = self.add_weight(shape=(self.hidden, 1), name='W',initializer=self.W_initializer,regularizer=self.W_regularizer,constraint=self.W_constraint)
-        self.b0 = self.add_weight(shape=(self.hidden,),name='b0',initializer=self.W_initializer)
-        self.b = self.add_weight(shape=(1,),name='b',initializer=self.W_initializer)
+        self.W0 = self.add_weight(shape=(input_dim, self.hidden), name='W0',initializer=self.init,regularizer=self.W0_regularizer,constraint=self.W0_constraint)
+        self.W = self.add_weight(shape=(self.hidden, 1), name='W',initializer=self.init,regularizer=self.W_regularizer,constraint=self.W_constraint)
+        self.b0 = self.add_weight(shape=(self.hidden,),name='b0',initializer=self.init)
+        self.b = self.add_weight(shape=(1,),name='b',initializer=self.init)
         self.built = True
     
     def call(self,x,mask=None):
+            super(Attention, self).call(x)
             energy = self.activation(K.dot(x, self.W0)+self.b0)
             #energy=self.activation(K.dot(energy, self.W) + self.b)
             energy=K.dot(energy, self.W) + self.b
@@ -39,12 +42,25 @@ class Attention(Layer):
     def compute_output_shape(self, input_shape):
         return (input_shape[0], input_shape[-1])
     
+    def get_config(self):
+        config = {
+            'hidden':self.hidden,
+            'activation': activations.serialize(self.activation),
+            'init': initializers.serialize(self.init),
+            'W_regularizer': regularizers.serialize(self.W_regularizer),
+            'W0_regularizer': regularizers.serialize(self.W0_regularizer),
+            'W_constraint':constraints.serialize(self.W_constraint),
+            'W0_constraint':constraints.serialize(self.W0_constraint)
+        }
+        base_config = super(Attention, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
 
 
 class myFlatten(Layer):
     def __init__(self,mydeletedim, **kwargs):
-        self.mydeletedim=mydeletedim
         super(myFlatten, self).__init__(**kwargs)
+        self.mydeletedim=mydeletedim
+    
     
     def compute_output_shape(self,input_shape):
         return (input_shape[0], np.prod(input_shape[1:]))
@@ -53,3 +69,9 @@ class myFlatten(Layer):
         x=x[:,:self.mydeletedim]
         return K.batch_flatten(x)
     
+    def get_config(self):
+        config = {
+            'mydeletedim':self.mydeletedim
+        }
+        base_config = super(myFlatten, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
